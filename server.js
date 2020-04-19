8a944935b185b13613b8b9c35daf0f354072aeb3
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-04-19 15:46:38 -0300

    Registra model na aplicação

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ mongoose.connect('mongodb://localhost:27017/nodeapi', {
   useUnifiedTopology: true
 })
 
+require('./src/models/Product')
+
 // Cria a primeira rota
 app.get('/', (req, res) => {
   res.send("Hello teste")
