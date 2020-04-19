a6eae568395a6602828ca72a9246b392212bc2ea
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-04-19 18:17:59 -0300

    Cria método update product

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -18,5 +18,10 @@ module.exports = {
     return res.json(product)
   },
 
+  async update(req, res){
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    return res.json(product)    
+  }
+
 
 }
