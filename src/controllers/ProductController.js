cbfd5ce94bfc4deffd391ceca9890b3d1851f0da
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-04-19 18:23:30 -0300

    Cria método de destroy product

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -21,6 +21,11 @@ module.exports = {
   async update(req, res){
     const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true })
     return res.json(product)    
+  },
+
+  async destroy(req, res){
+    await Product.findByIdAndRemove(req.params.id)
+    return res.send()
   }
 
 
