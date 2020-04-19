0116a4e6746471943e776cd1b1f3b79599d118e9
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-04-19 18:07:12 -0300

    Cria rota de store de um product

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -6,4 +6,9 @@ module.exports = {
     const products = await Product.find()
     return res.json(products)
   },
+  
+  async store(req, res){
+    const product = await Product.create(req.body)
+    return res.json(product)
+  }
 }
