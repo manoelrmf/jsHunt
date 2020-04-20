0155e861d17c5d755b9de0d8c605aa98356b5e4d
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-04-20 20:44:08 -0300

    Altera método de recuperação dos produtos

--- frontend/src/pages/main/index.js
+++ frontend/src/pages/main/index.js
@@ -5,12 +5,32 @@ import './index.css'
 
 function Main() {
   const [products, setProducts] = useState([])
-
+  const [productInfo, setProductInfo] = useState({})
+  const [page, setPage] = useState(1)
   useEffect(() => {
-    api.get('/products').then(response => {
-      setProducts(response.data.docs)
-    })
+    loadProducts(page)
   }, [])
+
+  async function loadProducts(page) {
+    const response = await api.get(`/products?page=${page}`)
+    const { docs, ...productInfo } = response.data
+
+    setProducts(docs)
+    setProductInfo(productInfo)
+    setPage(page)
+  }
+
+  function prevPage() {
+    if(page === 1) return
+    const pageNumber = page - 1
+    loadProducts(pageNumber)
+  }
+
+  function nextPage() {
+    if (page === parseInt(productInfo.pages)) return
+    const pageNumber = page + 1
+    loadProducts(pageNumber)
+  }
 
 
   return (
@@ -19,13 +39,13 @@ function Main() {
         <article key={produto._id}>
           <strong>{produto.title}</strong>
           <p>{produto.description}</p>
-        
+
           <a href="">Acessar</a>
         </article>
       ))}
       <div className="actions">
-        <button>Anterior</button>
-        <button>Próxima</button>
+        <button disabled={page === 1} type="button" onClick={prevPage}>Anterior</button>
+        <button disabled={page === parseInt(productInfo.pages)} type="button" onClick={nextPage}>Próxima</button>
       </div>
     </div>
   );
