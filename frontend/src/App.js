51eaedbae35e8d07413927892028cd2608958aee
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-04-20 20:57:02 -0300

    Importa routes no app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header'
 import Main from './pages/main'
-
+import Routes from './routes'
 
 import './App.css'
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
      <Header />
-     <Main />
+     <Routes />
     </div>
   );
 }
