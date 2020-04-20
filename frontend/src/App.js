ae01caaa720af87bedceb1a4df8988de3249e85c
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-04-20 11:19:25 -0300

    Cria css global e reseta css padrão do html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from './components/Header'
+
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello Word</h1>
+     <Header />
     </div>
   );
 }
