7cccb179505afb7f65439e629b1ba0b6cfafb45a
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-04-20 20:59:53 -0300

    Cria routa de product

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Main from './pages/main'
+import Product from './pages/product'
 
 export default function Routes(){
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Main} />
+                <Route path="/product/:id"  component={Product} />
             </Switch>
         </BrowserRouter>
     )
