f27533bdc66f870aafc1f959d952805138eb01c3
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-04-21 16:15:26 -0300

    Consulta api e exibe products

--- mobile/src/pages/main/index.js
+++ mobile/src/pages/main/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import './../../config/StatusBarConfig'
+import api from './../../services/api'
+
 export default function Main() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    loadProducts()
+  }, [])
+
+  async function loadProducts() {
+    const response = await api.get(`/products?page=1`)
+    const { docs } = response.data
+    setProducts(docs)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Main</Text>
+      <Text>Página Main</Text>
+      {products.map(product => (
+        <Text key={product._id}>{product.title}</Text>
+      ))}
     </View>
   );
 }
@@ -12,8 +29,7 @@ export default function Main() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "flex-start"
   },
 });
