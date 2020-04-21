aa4278a2c2c3ec20f7c2c1247efedac8c7f885a9
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-04-20 21:27:39 -0300

    Estrutura page de product

--- frontend/src/pages/product/index.js
+++ frontend/src/pages/product/index.js
@@ -1,1 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import api from './../../services/api'
+import { useParams } from 'react-router-dom'
+import './index.css'
+
+function Product() {
+  const [product, setProduct] = useState({})
+  const { id } = useParams()
+
+  useEffect(() => {
+
+    api.get(`/products/${id}`).then(response => {
+      setProduct(response.data)
+    })
+  }, [])
+
+  return (
+    <div className="product-info">
+      <h1>{product.title}</h1>
+      <p>{product.description}</p>
+
+      <p>URL: <a href={product.url}>{product.url}</a></p>
+    </div>
+  )
+}
+
+export default Product
